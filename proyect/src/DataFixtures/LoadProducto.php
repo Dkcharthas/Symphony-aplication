562b67bee58a2716110c0d1d66a7770ea1949bc6
Dkcharthas <dkcharthashalo@gmail.com>
2020-08-14 20:50:10 -0500

    Subida del back

---
+++ proyect/src/DataFixtures/LoadProducto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Producto;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadProducto extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $arrayParametro = array(
+            array("Refresco","Coca-Cola 600 ml", 34),
+            array("Refresco","Coca-Cola 800 ml", 50)
+        );
+        // $product = new Product();
+        // $manager->persist($product);
+
+       for($i=0;$i<sizeof($arrayParametro); $i++) {
+			$parametro = new Producto();
+			$parametro->setNombre( $arrayParametro[$i][0] );
+			$parametro->setDescripcion( $arrayParametro[$i][1]);
+			$parametro->setPrecio($arrayParametro[$i][2]);
+			$manager->persist($parametro);
+        	$manager->flush();
+        }
+    }
+}
---
+++ proyect/src/Entity/Movie.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sinopsis;
+
+     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSinopsis(): ?string
+    {
+        return $this->sinopsis;
+    }
+
+    public function setSinopsis(string $sinopsis): self
+    {
+        $this->sinopsis = $sinopsis;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+       
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
